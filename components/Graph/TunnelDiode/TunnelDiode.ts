bcc124313a2bb1774e638081dfa794285e8afe41
Fan <36140830+fan2015zheng@users.noreply.github.com>
2022-01-10 14:47:12 -0600

    Deploy error fix

---
+++ components/Graph/TunnelDiode/TunnelDiode.ts
@@ -0,0 +1,4 @@
+
+export class TunnelDiode {
+  
+}
--- pages/logisticmap/index.tsx
+++ pages/logisticmap/index.tsx
@@ -1,23 +1,7 @@
 import Head from 'next/head'
-import { useState } from 'react'
-import { Canvas } from "../../components/Canvas/Canvas"
-import { ControlPanel } from '../../components/Canvas/ControlPanel'
-import Coordinate from '../../components/Canvas/Cooridnate'
-import LogisticMap from '../../components/Canvas/LogisticMap'
-import { HDiv } from '../../components/Control/Div/Div'
 import LogisticMapPage from '../../components/LogisticMapPage/LogisticMapPage'
-import cl from "./index.module.scss"
 
 export default function LogisticMapHome() {
-
-  const [coordinate, setCoordinate] = useState<Coordinate>(new Coordinate(0,0,0,0,0,0,0,0,0,0))
-  const [logisticMap, setLogisticMap] = useState<LogisticMap>(new LogisticMap(-1,-1))
-
-  const apply = (coordinate: Coordinate, logisticMap: LogisticMap) => {
-    setCoordinate(coordinate)
-    setLogisticMap(logisticMap)
-  }
-
   return (<>
     <Head>
       <title>Logistic Map</title>
